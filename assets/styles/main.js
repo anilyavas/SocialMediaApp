b5dda23e275dfac7f10564148ca41798f1553811
Anıl <anil@Anl-MacBook-Air.local>
2023-08-21 11:55:17 +0300

    updated project

--- assets/styles/main.js
+++ assets/styles/main.js
@@ -1,49 +1,7 @@
 import {StyleSheet} from 'react-native';
-import {verticalScale, horizontalScale, scaleFontSize} from './scaling';
-const style = StyleSheet.create({
-  header: {
-    paddingTop: verticalScale(30),
-    paddingLeft: horizontalScale(17),
-    paddingRight: horizontalScale(26),
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  messageIcon: {
-    backgroundColor: '#F9FAFB',
-    padding: horizontalScale(12),
-    borderRadius: horizontalScale(100),
-  },
-  messageNumberContainer: {
-    width: horizontalScale(11),
-    height: verticalScale(10),
-    backgroundColor: '#F35BAC',
-    borderRadius: horizontalScale(11),
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute',
-    right: horizontalScale(7),
-    top: verticalScale(8),
-  },
-  messageNumber: {
-    fontSize: scaleFontSize(6),
-    fontFamily: 'Inter',
-    lineHeight: scaleFontSize(7),
-    fontWeight: '600',
-    color: '#FFFFFF',
-  },
-  userStoryContainer: {
-    paddingHorizontal: horizontalScale(26),
-    marginTop: verticalScale(12),
-    height: 100,
-  },
-  userPostContainer: {
-    marginTop: verticalScale(30),
-    flex: 1,
-    paddingHorizontal: horizontalScale(22),
+const globalStyle = StyleSheet.create({
+  backgroundWhite: {
+    backgroundColor: '#FFFFFF',
   },
 });
-export default style;
+export default globalStyle;
---
+++ screens/Profile/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+const Profile = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>This is a profile page</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Profile;
